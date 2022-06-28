279d4dbd45ef7cfaf680869bcad084c3fd092356
haha459862 <459862@163.com>
2022-06-28 22:32:25 +0800

    live bugs fixes

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/LiveChannelAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/LiveChannelAdapter.java
@@ -26,10 +26,11 @@ public class LiveChannelAdapter extends BaseQuickAdapter<LiveChannel, BaseViewHo
         TextView tvChannel = helper.getView(R.id.tvChannel);
         tvChannelNum.setText(String.format("%s", item.getChannelNum()));
         tvChannel.setText(item.getChannelName());
-        if (item.isDefault()) {
+        if (item.isSelected() && !item.isFocused()) {
             tvChannelNum.setTextColor(mContext.getResources().getColor(R.color.color_1890FF));
             tvChannel.setTextColor(mContext.getResources().getColor(R.color.color_1890FF));
-        } else {
+        }
+        else{
             tvChannelNum.setTextColor(Color.WHITE);
             tvChannel.setTextColor(Color.WHITE);
         }
