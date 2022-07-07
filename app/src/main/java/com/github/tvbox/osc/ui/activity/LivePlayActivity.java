a5c9cf75fc49753d600163907c02fec8f73658d1
haha459862 <459862@163.com>
2022-07-07 20:10:47 +0800

    live channel list empty bug fix

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -115,8 +115,6 @@ public class LivePlayActivity extends BaseActivity {
         initSettingItemView();
         initLiveChannelList();
         initLiveSettingGroupList();
-        showTime();
-        showNetSpeed();
     }
 
     @Override
@@ -825,6 +823,8 @@ public class LivePlayActivity extends BaseActivity {
         }
 
         livePlayerManager.init(mVideoView);
+        showTime();
+        showNetSpeed();
         tvLeftChannelListLayout.setVisibility(View.INVISIBLE);
         tvRightSettingLayout.setVisibility(View.INVISIBLE);
 
@@ -917,6 +917,7 @@ public class LivePlayActivity extends BaseActivity {
     private Runnable mUpdateNetSpeedRun = new Runnable() {
         @Override
         public void run() {
+            if (mVideoView == null) return;
             tvNetSpeed.setText(String.format("%.2fMB/s", (float)mVideoView.getTcpSpeed() / 1024.0 / 1024.0));
             mHandler.postDelayed(this, 1000);
         }
