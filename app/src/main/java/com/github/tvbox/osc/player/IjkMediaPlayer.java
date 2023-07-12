7a0cfd6b7553318300ab441d692d17c9f82a10b4
于俊 <215613905@qq.com>
2023-07-12 17:59:35 +0800

    修bug

--- app/src/main/java/com/github/tvbox/osc/player/IjkMediaPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/IjkMediaPlayer.java
@@ -99,14 +99,15 @@ public class IjkMediaPlayer extends IjkPlayer {
             if (headers.size() > 0) {
                 StringBuilder sb = new StringBuilder();
                 for (Map.Entry<String, String> entry : headers.entrySet()) {
-                    sb.append(entry.getKey());
-                    sb.append(":");
                     String value = entry.getValue();
-                    if (!TextUtils.isEmpty(value))
-                        sb.append(entry.getValue());
-                    sb.append("\r\n");
-                    mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "headers", sb.toString());
+                    if (!TextUtils.isEmpty(value)) {
+                        sb.append(entry.getKey());
+                        sb.append(": ");
+                        sb.append(value);
+                        sb.append("\r\n");
+                    }
                 }
+                mMediaPlayer.setOption(tv.danmaku.ijk.media.player.IjkMediaPlayer.OPT_CATEGORY_FORMAT, "headers", sb.toString());
             }
         }
     }
