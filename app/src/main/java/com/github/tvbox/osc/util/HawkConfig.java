8ff3d96641b31579d0eb0edc4d85204dfd1ca8ed
watson1982 <112817572+watson1982@users.noreply.github.com>
2023-08-21 17:56:12 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -38,4 +38,5 @@ public class HawkConfig {
     public static final String NOW_DATE = "now_date"; //当前日期
     public static final String REMOTE_TVBOX = "remote_tvbox_host";
     public static final String IJK_CACHE_PLAY = "ijk_cache_play";
+    public static boolean hotVodDelete;
 }
